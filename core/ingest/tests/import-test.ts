e860dad9ee1b78ec1721d272fb7daa871fa4aa83
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-20 21:01:20 +0000

    First go at testing custom imports

--- core/ingest/tests/import-test.ts
+++ core/ingest/tests/import-test.ts
@@ -5,6 +5,25 @@ import path from "path";
 
 import { Import } from '../src/main.js';
 
+test("init import with custom module", async (t) => {
+  const importer = new Import();
+  console.log(importer)
+
+  const args: string[] = ["../../../ingest/tests/custom-module/test.js"];
+  const options: any = { "openaiApiKey": "test" };
+  const command: string = "";
+
+  const output = await importer.run({ args, options, command })
+
+  for await (const item of output) {
+    console.log(item);
+  }
+
+  t.not(output, null);
+  t.pass()
+});
+
+
 test("init import", (t) => {
   const importer = new Import();
   t.not(importer, null);
